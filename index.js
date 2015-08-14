d9b094e26cad7ee66b1ffa391f04e8306e9b14cc
Tim Barclay <tbarclay@scottlogic.com>
2015-08-14 15:53:11 +0100

    Added dynamic favicon

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ function setupPage(){
   var cycleLength = 14;
   var day = daysFromSeed(new Date(), seedDate, cycleLength);
   var itsOn = day <= closeDayNumber;
+  var remaining;
 
   document.getElementsByClassName("yes-no")[0].innerHTML = itsOn ? "Yes" : "No";
   document.getElementsByClassName("more-info")[0].innerHTML = itsOn ?
     "Look at the <a href='http://www.marksandspencer.com/s/food-and-wine/dine-in'>menu</a>" :
     "I'm so sorry";
 
-  var remaining;
   if(itsOn){
     remaining = day === closeDayNumber ? "Hurry up, it ends today!" :
       "It ends in " + (closeDayNumber - day) + " days";
@@ -25,6 +25,36 @@ function setupPage(){
     remaining = "It's on again in " + (cycleLength - day) + " days";
   }
   document.getElementsByClassName("remaining")[0].innerHTML = remaining;
+
+  createFavicon(itsOn);
+}
+
+/**
+ * With many hat tips to https://remysharp.com/2010/08/24/dynamic-favicons
+ */
+function createFavicon(itsOn){
+  var canvas = document.createElement("canvas");
+  var favicon = document.getElementById("favicon");
+  var text = itsOn ? "Yes" : "No";
+  var context;
+  var x;
+
+  if(!canvas.getContext)
+    return;
+
+  canvas.height = canvas.width = 16;
+  x = canvas.width / 2;
+
+  context = canvas.getContext("2d");
+  context.fillStyle = "#fff";
+  context.fillRect (0, 0, 16, 16);
+  
+  context.font = "10px 'Calibri', 'Arial', sans-serif";
+  context.fillStyle = "#000";
+  context.textAlign = "center";
+  context.fillText(text, x, 11);
+
+  favicon.href = canvas.toDataURL("image/png");
 }
 
 /**
